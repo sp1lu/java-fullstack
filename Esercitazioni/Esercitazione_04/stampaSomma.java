4d3012ff17f63e85118c7e984f70e2c667552b62
sp1lu <davide.rivolta@yahoo.it>
2023-05-12 12:37:29 +0200

    Folder added

---
+++ Esercitazioni/Esercitazione_04/stampaSomma.java
@@ -0,0 +1,11 @@
+public class stampaSomma {
+
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+
+        System.out.println("Output: %s".formatted(c));
+    }
+}
