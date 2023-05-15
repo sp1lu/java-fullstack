329b584c728c233bed85a8694cae50be67c00c5c
sp1lu <davide.rivolta@yahoo.it>
2023-05-15 10:27:38 +0200

    Old version removed

--- Esercitazioni/Esercitazione_05/calcArea.java
+++ Esercitazioni/Esercitazione_05/calcArea.java
@@ -1,61 +1,3 @@
-/* import java.util.InputMismatchException;
-import java.util.Scanner;
-
-public class calcArea {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        while (true) {
-
-            Double width;
-            Double length;
-
-            try {
-                System.out.println("Inserire il valore della base:");
-                width = input.nextDouble();
-
-                System.out.println("Inserire il valore dell'altezza:");
-                length = input.nextDouble();
-
-                input.close();
-
-                if (width >= 400 || length >= 400) {
-                    System.out.println("--------------");
-                    System.out.println("Esagerato! Non inserire valori superiori a 400");
-                    System.out.println("--------------");
-
-                } else if (width <= 0 || length <= 0) {
-                    System.out.println("--------------");
-                    System.out.println("Non posso calcolare aree negative");
-                    System.out.println("--------------");
-                }
-
-                else {
-                    Double area = width * length;
-
-                    System.out.println("--------------");
-                    System.out.println("Area = %f".formatted(area));
-                    System.out.println("--------------");
-                }
-
-                break;
-
-            } catch (InputMismatchException e) {
-
-                System.out.println("------------------");
-                System.out.println("Non hai inserito un numero");
-                System.out.println("------------------");
-
-                input.next();
-
-            }
-
-        }
-
-    }
-
-} */
-
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -70,6 +12,7 @@ public class calcArea {
 
             try {
                 System.out.println("Inserire il valore della base:");
+
                 width = input.nextDouble();
 
                 if (width >= 400) {
@@ -105,6 +48,7 @@ public class calcArea {
 
             try {
                 System.out.println("Inserire il valore dell'altezza:");
+
                 length = input.nextDouble();
 
                 if (length >= 400) {
