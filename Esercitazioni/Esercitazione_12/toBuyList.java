9ad8139cbcc7b6fab1e474cb449420d920ee30c2
sp1lu <davide.rivolta@yahoo.it>
2023-05-17 14:28:35 +0200

    Versione 4.0

--- Esercitazioni/Esercitazione_12/toBuyList.java
+++ Esercitazioni/Esercitazione_12/toBuyList.java
@@ -11,7 +11,8 @@ public class toBuyList {
         System.out.println("-------------------------");
         System.out.println("Fai la lista della spesa");
         System.out.println("end per uscire");
-        System.out.println("c per svuotare. f per cancellare l'ultimo elemento. d per rimuovere");
+        System.out.println("list per vedere gli elementi attualmente nella lista");
+        System.out.println("c per svuotare. f per cancellare l'ultimo elemento. d per rimuovere.");
 
         List<String> toBuyList = new ArrayList<String>();
 
@@ -40,38 +41,66 @@ public class toBuyList {
                 System.out.println("-------------------------");
 
             } else if (userInput.equals("d") || userInput.equals("D")) {
+
+                if (toBuyList.size() == 0) {
+                    System.out.println("La lista della spesa vuota. Nulla da rimuovere");
+                    System.out.println("Ricomincia ad aggiungere elementi");
+
+                    continue;
+
+                } else {
+                    System.out.println("-------------------------");
+                    System.out.println("Quale elemento vuoi rimuovere?");
+
+                    for (int i = 0; i < toBuyList.size(); i++) {
+                        System.out.println("#" + i + " " + toBuyList.get(i));
+                    }
+
+                    try {
+                        int toRemove = input.nextInt();
+
+                        if (toRemove > toBuyList.size()) {
+                            System.out.println("Elemento non esistente nella lista");
+
+                            continue;
+
+                        } else {
+
+                            System.out.println("-------------------------");
+                            System.out.println(toBuyList.get(toRemove) + " rimosso dalla lista");
+
+                            toBuyList.remove(toRemove);
+
+                            System.out.println("Lista aggiornata:");
+
+                            for (String listElement : toBuyList) {
+                                System.out.println(listElement);
+                            }
+
+                            System.out.println("-------------------------");
+                        }
+
+                    } catch (InputMismatchException e) {
+                        System.out.println("-------------------------");
+                        System.out.println("Non hai inserito un numero per scegliere cosa eliminare");
+                        System.out.println("Niente eliminato");
+                        System.out.println("Continua con la lista");
+
+                        input.next();
+
+                    }
+                }
+
+            } else if (userInput.equals("list")) {
                 System.out.println("-------------------------");
-                System.out.println("Quale elemento vuoi rimuovere?");
+                System.out.println("Lista attuale:");
 
                 for (int i = 0; i < toBuyList.size(); i++) {
                     System.out.println("#" + i + " " + toBuyList.get(i));
                 }
 
-                try {
-                    int toRemove = input.nextInt();
-
-                    System.out.println("-------------------------");
-                    System.out.println(toBuyList.get(toRemove) + " rimosso dalla lista");
-
-                    toBuyList.remove(toRemove);
-
-                    System.out.println("Lista aggiornata:");
-
-                    for (String listElement : toBuyList) {
-                        System.out.println(listElement);
-                    }
-
-                    System.out.println("-------------------------");
-
-                } catch (InputMismatchException e) {
-                    System.out.println("-------------------------");
-                    System.out.println("Non hai inserito un numero per scegliere cosa eliminare");
-                    System.out.println("Niente eliminato");
-                    System.out.println("Continua con la lista");
-
-                    input.next();
-
-                }
+                System.out.println("-------------------------");
+                System.out.println("Continua ad aggiungere elementi:");
 
             } else {
                 toBuyList.add(userInput);
